d71e6bf6ef79df39e2da6077805807c6526e1aac
kim-minh <100175752+kim-minh@users.noreply.github.com>
2022-10-07 21:56:48 +0700

    Improve keyboard support (2)

--- src/main/java/com/oop/bomberman/Bomberman.java
+++ src/main/java/com/oop/bomberman/Bomberman.java
@@ -5,8 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+
+import static com.oop.bomberman.control.Keyboard.keyboard;
 
 public class Bomberman extends Application {
     @Override
@@ -15,6 +16,8 @@ public class Bomberman extends Application {
         AnchorPane root = fxmlLoader.load();
 
         Scene scene = new Scene(root, 800, 600);
+        keyboard.handle(scene);
+
         stage.setTitle("Bomberman");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/oop/bomberman/control/Keyboard.java
+++ src/main/java/com/oop/bomberman/control/Keyboard.java
@@ -15,20 +15,20 @@ public class Keyboard {
         scene.setOnKeyPressed(event -> {
             pressed = true;
             switch (event.getCode()) {
-                case W -> goUp = true;
-                case S -> goDown = true;
-                case A -> goLeft = true;
-                case D -> goRight = true;
+                case W, UP -> goUp = true;
+                case S, DOWN -> goDown = true;
+                case A, LEFT -> goLeft = true;
+                case D, RIGHT -> goRight = true;
             }
         });
 
         scene.setOnKeyReleased(event -> {
             pressed = false;
             switch (event.getCode()) {
-                case W -> goUp = false;
-                case S -> goDown = false;
-                case A -> goLeft = false;
-                case D -> goRight = false;
+                case W, UP -> goUp = false;
+                case S, DOWN -> goDown = false;
+                case A, LEFT -> goLeft = false;
+                case D, RIGHT -> goRight = false;
             }
         });
     }
