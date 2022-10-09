cf0febb777c5c104b8a18dd0f4fb691eaa6b9dc2
kim-minh <100175752+kim-minh@users.noreply.github.com>
2022-10-09 12:49:00 +0700

    Add entity speed and update collision logic

--- src/main/java/com/oop/bomberman/entities/AnimatedEntity.java
+++ src/main/java/com/oop/bomberman/entities/AnimatedEntity.java
@@ -2,47 +2,53 @@ package com.oop.bomberman.entities;
 
 import com.oop.bomberman.control.Coordinate;
 import com.oop.bomberman.graphics.Sprite;
-import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 
-public class AnimatedEntity extends Entity {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AnimatedEntity extends Entity {
+    protected List<List<Sprite>> spritesList;
     protected boolean goUp;
     protected boolean goDown;
     protected boolean goLeft;
     protected boolean goRight;
     protected boolean isMoving;
+    protected double speed;
     private int movement = 3;
-    private final double size = Sprite.SCALED_SIZE / 2.0;
-    private int frame = 0;
+    protected int frame = 0;
+    protected boolean isCollide;
 
     /**
      * Initialize object.
      *
      * @param coordinate coordinate
-     * @param canvas canvas
+     * @param gc         GraphicContext
      */
-    public AnimatedEntity(Coordinate coordinate, Canvas canvas) {
-        super(coordinate, canvas);
+    public AnimatedEntity(Coordinate coordinate, GraphicsContext gc) {
+        super(coordinate, gc);
+        spritesList = new ArrayList<>();
     }
 
-    public void animate() {
+    public void update() {
         clear();
 
-        int dx = 0, dy = 0;
+        double dx = 0, dy = 0;
 
         if (goUp) {
-            dy -= 2;
+            dy -= speed;
             movement = 0;
         }
         if (goDown) {
-            dy += 2;
+            dy += speed;
             movement = 1;
         }
         if (goLeft) {
-            dx -= 2;
+            dx -= speed;
             movement = 2;
         }
         if (goRight)  {
-            dx += 2;
+            dx += speed;
             movement = 3;
         }
         moveBy(dx, dy);
@@ -57,39 +63,47 @@ public class AnimatedEntity extends Entity {
     private void animateMove() {
         ++frame;
         if (frame >= 5) {
-            spriteIndex = ++spriteIndex % sprites.get(movement).size();
+            spriteIndex = ++spriteIndex % spritesList.get(movement).size();
             frame = 0;
         }
     }
 
-    private void moveBy(int dx, int dy) {
+    private void moveBy(double dx, double dy) {
         if (dx == 0 && dy == 0) return;
 
-        double x = size + coordinate.getX() + dx;
-        double y = size + coordinate.getY() + dy;
+        double x = coordinate.getX() + dx;
+        double y = coordinate.getY() + dy;
 
-        moveTo(x, y);
-    }
-
-    private void moveTo(double x, double y) {
-        double width = canvas.getWidth();
-        double height = canvas.getHeight();
-
-        if (x - size >= 0 &&
-                x + size <= width &&
-                y - size >= 0 &&
-                y + size <= height) {
-            coordinate.relocate(x - size, y - size);
+        if(!canMove(x, y)) {
+            return;
         }
+
+        coordinate.relocate(x, y);
     }
 
-    @Override
-    public boolean collide(Entity e) {
-        return false;
+    public boolean canMove(double x, double y) {
+        for (Entity entity : entities) {
+            if (collide(entity, x, y)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean collide(Entity e, double x, double y) {
+        if (this == e) {
+            return false;
+        }
+        Coordinate other = e.getCoordinate();
+        isCollide = x < other.getX() + Sprite.SCALED_SIZE &&
+                x + Sprite.SCALED_SIZE > other.getX() &&
+                y < other.getY() + Sprite.SCALED_SIZE &&
+                y + Sprite.SCALED_SIZE > other.getY();
+        return isCollide;
     }
 
     @Override
     public void render() {
-        gc.drawImage(sprites.get(movement).get(spriteIndex).getTexture(), coordinate.getX(), coordinate.getY());
+        gc.drawImage(spritesList.get(movement).get(spriteIndex).getTexture(), coordinate.getX(), coordinate.getY());
     }
 }
--- src/main/java/com/oop/bomberman/entities/Entity.java
+++ src/main/java/com/oop/bomberman/entities/Entity.java
@@ -2,7 +2,6 @@ package com.oop.bomberman.entities;
 
 import com.oop.bomberman.control.Coordinate;
 import com.oop.bomberman.graphics.Sprite;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 
 import java.util.ArrayList;
@@ -10,34 +9,29 @@ import java.util.List;
 
 public abstract class Entity {
     protected Coordinate coordinate;
-
-    protected final List<List<Sprite>> sprites;
+    public static List<Entity> entities = new ArrayList<>();
     protected int spriteIndex = 0 ;
-    protected Canvas canvas;
     protected GraphicsContext gc;
-
 
     /**
      * Initialize object.
      *
      * @param coordinate coordinate
-     * @param canvas canvas
+     * @param gc         GraphicContext
      */
-    public Entity(Coordinate coordinate, Canvas canvas) {
-        this.coordinate = new Coordinate(coordinate.getX() * Sprite.SCALED_SIZE,
-                coordinate.getY() * Sprite.SCALED_SIZE);
-        this.canvas = canvas;
-        gc = canvas.getGraphicsContext2D();
-        sprites = new ArrayList<>();
+    public Entity(Coordinate coordinate, GraphicsContext gc) {
+        this.coordinate = new Coordinate(coordinate.getX(), coordinate.getY());
+        this.gc = gc;
+        entities.add(this);
     }
 
     public Coordinate getCoordinate() {
         return coordinate;
     }
 
-    public abstract boolean collide(Entity e);
-
     public abstract void render();
+
+    public abstract void update();
 
     public void clear() {
         gc.clearRect(coordinate.getX(), coordinate.getY(), Sprite.SCALED_SIZE, Sprite.SCALED_SIZE);
