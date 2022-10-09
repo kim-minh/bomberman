c486f51eb715f1392e031ed6ecbbfcb8a2c3f2b3
kim-minh <100175752+kim-minh@users.noreply.github.com>
2022-10-09 12:51:23 +0700

    Set default background

--- src/main/java/com/oop/bomberman/Bomberman.java
+++ src/main/java/com/oop/bomberman/Bomberman.java
@@ -1,11 +1,12 @@
 package com.oop.bomberman;
 
-import com.oop.bomberman.control.Keyboard;
+import com.oop.bomberman.control.Control;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
 public class Bomberman extends Application {
@@ -13,10 +14,11 @@ public class Bomberman extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Bomberman.class.getResource("FXML/app-view.fxml"));
         AnchorPane root = fxmlLoader.load();
+        root.setStyle("-fx-background-color: #50a000");
 
         Scene scene = new Scene(root, 800, 600);
-        Keyboard keyboard = new Keyboard();
-        keyboard.handle(scene);
+        Control control = new Control();
+        control.handle(scene);
 
         stage.setTitle("Bomberman");
         stage.setScene(scene);
