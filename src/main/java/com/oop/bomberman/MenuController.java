6dc99e33141f3cfe6d6631f0b640a8ac9e54c81d
kim-minh <100175752+kim-minh@users.noreply.github.com>
2022-10-20 00:20:24 +0700

    Update menu

--- src/main/java/com/oop/bomberman/MenuController.java
+++ src/main/java/com/oop/bomberman/MenuController.java
@@ -2,18 +2,29 @@ package com.oop.bomberman;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class MenuController {
     @FXML
     private AnchorPane anchorPane;
+    @FXML
+    private ImageView imageView;
 
     @FXML
     private void start() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Bomberman.class.getResource("FXML/app-view.fxml"));
         anchorPane.getChildren().setAll((AnchorPane)fxmlLoader.load());
         anchorPane.setStyle("-fx-background-color: #000000");
+    }
+
+    @FXML
+    private void initialize() {
+        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("textures/menuBackground.png")));
+        imageView.setImage(image);
     }
 }
